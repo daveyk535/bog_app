c9aa79bb71170839ec0440983c10e07c05b77421
daveyk535 <dave@phase2minds.com>
2014-04-23 09:28:42 -0700

    submit lab. basic features working.

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,19 @@
+class CreaturesController < ApplicationController
+	
+	def index
+		@creatures = Creature.all
+		render :index
+	end
+
+	def new
+		render :new
+	end
+
+	def create
+		creature = params.require(:creature).permit(:name, :description)
+		Creature.create(creature)
+		redirect_to "/creatures"
+	end
+
+end
+
